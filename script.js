f981c2ab56861d2cbc8f4000ddd63dc28ef92ec7
jinofolio <148406534+jinofolio@users.noreply.github.com>
2023-10-25 16:35:49 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,20 +3,75 @@ const items = document.querySelectorAll(".item");
 
 filterTtl.addEventListener("click", () => {
     filterTtl.classList.toggle("open");
-}) 
+});
 
 items.forEach(item => {
     item.addEventListener("click", () => {
+        // Menghapus kelas "checked" dari semua item sebelumnya
+        items.forEach(otherItem => {
+            if (otherItem !== item) {
+                otherItem.classList.remove("checked");
+            }
+        });
         item.classList.toggle("checked");
+    });
+});
 
-        let checked = document.querySelectorAll(".checked"),
-            btnText = document.querySelector(".filter-tittle")
 
-        if(checked && checked.length > 0) {
-            btnText.innerText = `${checked.length} Selected`;
-        } else {
-            btnText.innerText = `${checked.length} Selected`
+// FILTERMAP
+
+
+
+const filterLok = document.querySelectorAll(".card");
+const selectedLok = document.querySelectorAll(".cardimg");
+
+const filterLokasi = (e) => {
+
+    document.querySelectorAll(".btnaktif").forEach(btn => {
+        btn.classList.remove("btnaktif");
+    });
+
+    e.target.classList.add("btnaktif");
+
+    selectedLok.forEach(cardimg => {
+        cardimg.classList.add("hide");
+
+        if (e.target.dataset.name === "all" || cardimg.getAttribute("data-name2") === e.target.dataset.name) {
+            cardimg.classList.remove("hide");
         }
-    })
-})
+    });
+};
+
+filterLok.forEach((card) => {
+    card.addEventListener("click", filterLokasi);
+});
+
+
+
+
+
+
+
+// FILTERIMG
+
+const filterImg  = document.querySelectorAll(".item");
+const selectedImg = document.querySelectorAll(".cardimg");
+
+const filterImages = (e) => {
+    document.querySelector(".itemaktif").classList.remove("itemaktif");
+    e.target.classList.add("itemaktif")
+    
+    selectedImg.forEach(cardimg => {
+    cardimg.classList.add("hide");
+
+    if (e.target.dataset.name === "all" || cardimg.getAttribute("data-name1") === e.target.dataset.name) {
+        cardimg.classList.remove("hide");
+    }
+});
+};
+
+filterImg.forEach((item) => {
+    item.addEventListener("click", filterImages);
+});
+
 
